d72fe37623b15703f293248b02ed95d57f32224a
정명주 <myeongju.jung@nhnent.com>
2014-12-01 13:48:38 +0900

    초기화

---
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -0,0 +1,101 @@
+package springbook.user.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.hsqldb.Server;
+
+import springbook.user.domain.User;
+
+public class UserDao {
+	public void add(User user) throws ClassNotFoundException, SQLException{
+		Class.forName("org.hsqldb.jdbcDriver");
+        Connection c = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/test", "sa", "");
+		
+		PreparedStatement ps = c.prepareStatement(
+				"insert into users(id, name, password) values (?, ?, ?)");
+		ps.setString(1, user.getId());
+		ps.setString(2, user.getName());
+		ps.setString(3, user.getPassword());
+		
+		ps.executeUpdate();
+		
+		ps.close();
+		c.close();
+	}
+	
+	public User get(String id) throws ClassNotFoundException, SQLException{
+		Class.forName("org.hsqldb.jdbcDriver");
+		Connection c = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/test", "sa", "");
+		
+		PreparedStatement ps = c.prepareStatement(
+				"select * from users where id = ?");
+		ps.setString(1, id);
+		
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+		User user = new User();
+		user.setId(rs.getString("id"));
+		user.setName(rs.getString("name"));
+		user.setPassword(rs.getString("password"));
+		
+		rs.close();
+		ps.close();
+		c.close();
+		
+		return user;
+	}
+	
+	/**
+	 * 검증용 main 코드
+	 */
+	public static void main(String[] args) throws ClassNotFoundException, SQLException{
+		// DB생성
+		initHsqldb();
+		
+		UserDao dao = new UserDao();
+		
+		User user = new User();
+		user.setId("whiteship");
+		user.setName("백기선");
+		user.setPassword("married");
+		
+		dao.add(user);
+		
+		System.out.println(user.getId() + " 등록 성공");
+		
+		User user2 = dao.get(user.getId());
+		System.out.println(user2.getName());
+		System.out.println(user2.getPassword());
+		
+		System.out.println(user2.getId() + " 조회 성공");
+	}
+	
+	public static void initHsqldb() throws ClassNotFoundException, SQLException{
+        // stub to get in/out of embedded db
+        Server hsqlServer = null;
+        Connection connection = null;
+        
+        hsqlServer = new Server();
+        hsqlServer.setLogWriter(null);
+        hsqlServer.setSilent(true);
+        hsqlServer.setDatabaseName(0, "test");
+        hsqlServer.setDatabasePath(0, "file:test");
+        hsqlServer.start();
+        
+
+        Class.forName("org.hsqldb.jdbcDriver");
+        connection = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/test", "sa", ""); // can through sql exception
+        connection.prepareStatement(
+        		"create table users(" +
+				"	id varchar(10) not null " +
+				"	,name varchar(20) not null " +
+				"	,password varchar(10) not null " +
+				"	,primary key (id) " +
+				")").execute();
+        
+	}
+}
---
+++ src/test/resources/hsql/schema.sql
@@ -0,0 +1,6 @@
+create table users(
+	id varchar(10) not null
+	,name varchar(20) not null
+	,password varchar(10) not null
+	,primary key (id)
+)
